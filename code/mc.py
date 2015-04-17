1b155b9fa35271bbd388a8b77e41e2872bd34267
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2015-04-16 22:02:59 -0500

    two different scripts, one does the mcmc, the other plots

--- code/mc.py
+++ code/mc.py
@@ -1,10 +1,6 @@
 from numpy import *
-import matplotlib
-matplotlib.use('Agg') 
-from matplotlib.pyplot import *
 from scipy.interpolate import interp1d
 import emcee
-import triangle
 import os
 
 #Constants
@@ -34,7 +30,7 @@ def model(logtau, vmax, theta, logT, voff, x_d, y_d):
 
     os.system( ('./analytic_solution.x %f %f %f > "./model.dat"')%(10.0**logtau, vmax, theta) )
 
-    mod = loadtxt('./model.dat')
+    mod = genfromtxt('./model.dat', skip_footer=1)
 
     x_m_i = mod[:,0]
     y_m_i = mod[:,1]
@@ -65,20 +61,6 @@ logT_0 = 4
 voff_0 = -11
 
 x_0, y_0 = model(logtau_0, vmax_0, theta_0, logT_0, voff_0, x_data, y_data)
-
-#Plot data and first guess
-
-fig = figure(figsize=(12,6))
-
-scatter(x_data, y_data, c='c', alpha=0.6, label='$\mathrm{LARS 02}$')
-plot(x_data, y_data, c='c', alpha=0.6)
-plot(x_0, y_0, c='b', alpha=0.6, label='$\mathrm{Rotation\ model}$', lw=2)
-
-xlabel('$\mathrm{Velocity (km/s)}$', fontsize=20)
-ylabel('$\mathrm{Intensity (Arbitrary Units)}$', fontsize=20)
-legend(loc='best', fontsize=20)
-
-savefig('./first_guess.png')
 
 #emcee functions
 
@@ -115,7 +97,7 @@ def lnprob(param, x_d, y_d):
 
 ndim = 5
 nwalkers = 10
-nsteps = 100
+nsteps = 10
 
 first_guess = [logtau_0, vmax_0, theta_0, logT_0, voff_0]
 
@@ -128,56 +110,13 @@ sampler.run_mcmc(pos, nsteps, rstate0=random.get_state())
 print("Done!")
 
 
-# Walkers plot
+# Saving results
 
-clf()
+samples_fc = sampler.flatchain
 
-fig, axes = subplots(5, 1, sharex=True, figsize=(12, 13))
-
-axes[0].plot(sampler.chain[:, :, 0].T, color="k", alpha=0.4)
-axes[0].yaxis.set_major_locator(MaxNLocator(5))
-axes[0].axhline(logtau_0, color="#888888", lw=2)
-axes[0].set_ylabel(r"$\log{\tau}$")
-
-axes[1].plot(sampler.chain[:, :, 1].T, color="k", alpha=0.4)
-axes[1].yaxis.set_major_locator(MaxNLocator(5))
-axes[1].axhline(vmax_0, color="#888888", lw=2)
-axes[1].set_ylabel("$v_{max}$")
-
-axes[2].plot(exp(sampler.chain[:, :, 2]).T, color="k", alpha=0.4)
-axes[2].yaxis.set_major_locator(MaxNLocator(5))
-axes[2].axhline(theta_0, color="#888888", lw=2)
-axes[2].set_ylabel(r"$\theta$")
-
-axes[3].plot(exp(sampler.chain[:, :, 3]).T, color="k", alpha=0.4)
-axes[3].yaxis.set_major_locator(MaxNLocator(5))
-axes[3].axhline(logT_0, color="#888888", lw=2)
-axes[3].set_ylabel("$\log{T}$")
-
-axes[4].plot(exp(sampler.chain[:, :, 3]).T, color="k", alpha=0.4)
-axes[4].yaxis.set_major_locator(MaxNLocator(5))
-axes[4].axhline(voff_0, color="#888888", lw=2)
-axes[4].set_ylabel("$\Delta v_{off}$")
-axes[4].set_xlabel("step number")
-
-fig.tight_layout(h_pad=0.0)
-
-savefig('./walkers.png')
+savetxt('sampler_flatchain.dat', samples_fc, delimiter=',')
 
 print("Mean acceptance fraction: {0:.3f}".format(mean(sampler.acceptance_fraction)))
-
-# Triangle plot
-
-samples = sampler.flatchain
-print(shape(samples))
-
-print(samples[-1,:])
-
-fig = triangle.corner(samples, labels=[r"$\log{\tau}$", "$v_{max}$", r"$\theta$", "$\log{T}$", "$\Delta v_{off}$"],
-                      truths=[logtau_0, vmax_0, theta_0, logT_0, voff_0])
-
-savefig('./parameters.png')
-
 
 #Links:
 # https://github.com/dfm/emcee/blob/master/examples/line.py
---
+++ code/plots.py
@@ -0,0 +1,120 @@
+from numpy import *
+from matplotlib.pyplot import *
+from scipy.interpolate import interp1d
+import triangle
+import os
+
+#Info
+nwalkers = 10
+nsteps = 100
+nparameters = 5
+
+z = 0.029836
+c = 299792.458
+
+#Best parameters
+logtau_b = 5
+vmax_b = 50
+theta_b = 45
+logT_b = 4
+voff_b = -11
+
+#Functions
+def model_to_data_interp(x_m, y_m, x_d):
+
+    f = interp1d(x_m, y_m, kind="linear")
+    x_m_new = x_d
+    y_m_new = f(x_d)
+
+    return x_m_new, y_m_new
+
+def normalization(y_d, y_m):
+
+    obs_int = sum(y_d)
+    teo_int = sum(y_m)
+    y_m = y_m*obs_int/teo_int
+
+    return y_m
+
+def model(logtau, vmax, theta, logT, voff, x_d, y_d):
+
+    os.system( ('./analytic_solution.x %f %f %f > "./model.dat"')%(10.0**logtau, vmax, theta) )
+
+    mod = genfromtxt('./model.dat', skip_footer=1)
+
+    x_m_i = mod[:,0]
+    y_m_i = mod[:,1]
+
+    v_th = 12.85*sqrt((10.0**logT)/(10.0**4.0))
+    x_m_i = x_m_i*v_th + voff
+
+    x_m, y_m = model_to_data_interp(x_m_i, y_m_i, x_d)
+
+    y_m = normalization(y_d, y_m)
+
+    return x_m, y_m
+
+#Data
+lars = loadtxt('./LARS02.txt')
+samples = loadtxt('./sampler_flatchain.dat', delimiter=',')
+
+x_data = lars[:-17,0] - c*z
+y_data = lars[:-17,1]
+
+x_model, y_model = model(logtau_b, vmax_b, theta_b, logT_b, voff_b, x_data, y_data)
+
+#Plot fit and original
+fig = figure(figsize=(12,6))
+
+scatter(x_data, y_data, c='c', alpha=0.6, label='$\mathrm{LARS 02}$')
+plot(x_data, y_data, c='c', alpha=0.6)
+plot(x_model, y_model, c='b', alpha=0.6, label='$\mathrm{Rotation\ model}$', lw=2)
+
+xlabel('$\mathrm{Velocity (km/s)}$', fontsize=20)
+ylabel('$\mathrm{Intensity (Arbitrary Units)}$', fontsize=20)
+legend(loc='best', fontsize=20)
+
+savefig('./mcmc.png')
+
+# Walkers plot
+clf()
+
+fig, axes = subplots(5, 1, sharex=True, figsize=(12, 13))
+
+axes[0].plot(samples[:,0].reshape(nsteps,nwalkers), color="k", alpha=0.4)
+axes[0].yaxis.set_major_locator(MaxNLocator(5))
+axes[0].axhline(logtau_b, color="#888888", lw=2)
+axes[0].set_ylabel(r"$\log{\tau}$")
+
+axes[1].plot(samples[:,1].reshape(nsteps,nwalkers), color="k", alpha=0.4)
+axes[1].yaxis.set_major_locator(MaxNLocator(5))
+axes[1].axhline(vmax_b, color="#888888", lw=2)
+axes[1].set_ylabel("$v_{max}$")
+
+axes[2].plot(samples[:,2].reshape(nsteps,nwalkers), color="k", alpha=0.4)
+axes[2].yaxis.set_major_locator(MaxNLocator(5))
+axes[2].axhline(theta_b, color="#888888", lw=2)
+axes[2].set_ylabel(r"$\theta$")
+
+axes[3].plot(samples[:,3].reshape(nsteps,nwalkers), color="k", alpha=0.4)
+axes[3].yaxis.set_major_locator(MaxNLocator(5))
+axes[3].axhline(logT_b, color="#888888", lw=2)
+axes[3].set_ylabel("$\log{T}$")
+
+axes[4].plot(samples[:,4].reshape(nsteps,nwalkers), color="k", alpha=0.4)
+axes[4].yaxis.set_major_locator(MaxNLocator(5))
+axes[4].axhline(voff_b, color="#888888", lw=2)
+axes[4].set_ylabel("$\Delta v_{off}$")
+axes[4].set_xlabel("step number")
+
+fig.tight_layout(h_pad=0.0)
+
+savefig('./walkers.png')
+
+
+# Triangle plot
+
+fig = triangle.corner(samples, labels=[r"$\log{\tau}$", "$v_{max}$", r"$\theta$", "$\log{T}$", "$\Delta v_{off}$"],
+                        truths=[logtau_b, vmax_b, theta_b, logT_b, voff_b])
+
+savefig('./parameters.png')
